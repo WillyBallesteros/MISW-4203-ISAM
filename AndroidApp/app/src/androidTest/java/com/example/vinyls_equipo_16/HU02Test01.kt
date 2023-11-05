2cfcd506179876a09354aee66e12c8f91fafa492
William Ernesto Ballesteros Blanco <ballesteroswilliam@live.com>
2023-11-05 17:18:47 -0500

    HU02 Add new test to check data in album detail (#63)

---
+++ AndroidApp/app/src/androidTest/java/com/example/vinyls_equipo_16/HU02Test01.kt
@@ -0,0 +1,93 @@
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.example.vinyls_equipo_16.R
+import com.example.vinyls_equipo_16.ui.MainActivity
+import com.example.vinyls_equipo_16.ui.adapters.AlbumsAdapter
+import com.example.vinyls_equipo_16.viewmodels.AlbumViewModel
+import junit.framework.TestCase.assertEquals
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.CoreMatchers.allOf
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class HU02Test01 {
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    private lateinit var idlingResource: AlbumIdlingResource
+
+    @Before
+    fun setUp() {
+        activityScenarioRule.scenario.onActivity { activity ->
+            val viewModel = ViewModelProvider(activity).get(AlbumViewModel::class.java)
+            idlingResource = AlbumIdlingResource(viewModel.dataLoaded)
+            IdlingRegistry.getInstance().register(idlingResource)
+        }
+    }
+
+    @Test
+    fun scrollToItem_clicksOnFirstItem_check_correct_data() {
+        onView(withId(R.id.albumsRv)).perform(
+            RecyclerViewActions.scrollToPosition<AlbumsAdapter.AlbumViewHolder>(0)
+        )
+        onView(allOf(withId(R.id.album_cover), isDescendantOfA(nthChildOf(withId(R.id.albumsRv), 0))))
+            .check(matches(isDisplayed()))
+
+        onView(allOf(withId(R.id.album_name), isDescendantOfA(nthChildOf(withId(R.id.albumsRv), 0))))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.albumsRv))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<AlbumsAdapter.AlbumViewHolder>(0, click()))
+
+        activityScenarioRule.scenario.onActivity { activity ->
+            val navController = activity.findNavController(R.id.nav_host_fragment)
+            assertEquals(navController.currentDestination?.id, R.id.albumDetailFragment)
+        }
+
+        onView(withId(R.id.name)).check(matches(isDisplayed()))
+        onView(withId(R.id.release_date)).check(matches(isDisplayed()))
+        onView(withId(R.id.description)).check(matches(isDisplayed()))
+        onView(withId(R.id.genre)).check(matches(isDisplayed()))
+
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(idlingResource)
+        idlingResource.cleanup()
+    }
+
+    private fun nthChildOf(parentMatcher: Matcher<View>, childPosition: Int): Matcher<View> {
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("with $childPosition child view of type parentMatcher")
+            }
+
+            override fun matchesSafely(view: View): Boolean {
+                if (view.parent !is ViewGroup) {
+                    return parentMatcher.matches(view.parent)
+                }
+
+                val group = view.parent as ViewGroup
+                return parentMatcher.matches(view.parent) && group.getChildAt(childPosition) == view
+            }
+        }
+    }
+}
